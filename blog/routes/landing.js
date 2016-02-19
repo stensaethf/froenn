950acb82e9e788f196d8b5bc4568a0e6af6233cf
Frederik Stensaeth <stensaethf@carleton.edu>
2016-02-18 20:38:22 -0600

    main page, contact, about routes basic

--- blog/routes/landing.js
+++ blog/routes/landing.js
@@ -2,7 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 module.exports = function() {
-	// code.
-	
+	/* GET home page. */
+  router.get('/', function(req, res) {
+    // code.
+  });
+
+	// contact.
+	router.get('/contact/', function(req, res) {
+    // code.
+  });
+
+  // contact -- redirect
+  router.get('/contact', function(req, res){
+    res.redirect(req.originalUrl+'/');
+  });
+
+  // about.
+	router.get('/about/', function(req, res) {
+    // code.
+  });
+
+  // about -- redirect
+  router.get('/about', function(req, res){
+    res.redirect(req.originalUrl+'/');
+  });
+
 	return router;
 };
