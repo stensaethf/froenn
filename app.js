9f15e6ee762a782e78ab52bd2089c8e903c054ab
Frederik Stensaeth <stensaethf@carleton.edu>
2016-03-02 22:19:44 -0600

    app.js update

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// Models
+require('./models/post');
+require('./models/user');
+var User = mongoose.model('User');
+var Post = mongoose.model('Post');
 
 var app = express();
 
@@ -20,8 +27,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 var index = require('./routes/index');
+var blog = require('./routes/blog');
 
 app.use('/', index);
+app.use('/blog', blog);
 
 // Handle 404
 app.use(function(req, res) {
@@ -39,37 +48,5 @@ app.use(function(error, req, res, next) {
   console.log(error);
   res.render('500.jade', renderObj);
 });
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// production error handler
-// no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
-
 
 module.exports = app;
