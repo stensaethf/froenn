37237b95ce7d43a70ed877ad785990de92065aef
Frederik Stensaeth <stensaethf@carleton.edu>
2016-03-23 20:29:07 -0500

    index route cleaned

--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,6 @@ var express = require('express');
 var router = express.Router();
 var moment = require("moment");
 
-var config = require('../config');
-var email = require("emailjs");
-var server = email.server.connect({
-   user:    config.email.username,
-   password: config.email.password,
-   host:    config.email.host,
-   ssl:     true
-});
-
 // Models
 var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
@@ -40,57 +31,6 @@ module.exports = function() {
         res.send(html);
       });
     });
-  });
-
-  // contact.
-  router.get('/contact/', function(req, res) {
-    res.render('contact', {});
-  });
-
-  // contact -- redirect
-  router.get('/contact', function(req, res){
-    res.redirect(req.originalUrl+'/');
-  });
-
-  router.post('/contact/', function(req, res) {
-    var email = req.body.email;
-    var email_text = req.body.text;
-    email_text = email_text.replace(/\n/g, '\r\n');
-    email_text = email_text.replace(/\r\n/g, '<br/>');
-
-    var sendEmail = function() {
-      var e_text = util.format("<html><body style='font-family: Verdana'><table align='center' border='0' cellpadding='0' cellspacing='0' width='600'>" +
-                               email_text +
-                               "</body></html>",
-                               email_text);
-
-      var message = {
-        text: "Froenn email from " + email,
-        from: "Froenn <" + email + ">",
-        to: "frederikstensaeth@gmail.com",
-        subject: "Froenn email from " + email,
-        attachment:
-        [
-          {data: e_text, alternative: true}
-        ]
-      };
-      server.send(message, function(err, message) {
-        console.log(err || message);
-      });
-    };
-
-    sendEmail();
-    res.redirect(req.originalUrl);
-  });
-
-  // about.
-  router.get('/about/', function(req, res) {
-    res.render('about', {});
-  });
-
-  // about -- redirect
-  router.get('/about', function(req, res){
-    res.redirect(req.originalUrl+'/');
   });
 
   return router;
